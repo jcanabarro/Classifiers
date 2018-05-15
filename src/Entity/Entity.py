8c534602ccc89cfeed503f8e71666b2b11c8bac7
jcanabarro <joaovictorcana@gmail.com>
2018-05-14 21:29:16 -0300

    Concat and write data frames result and proba

--- src/Entity/Entity.py
+++ src/Entity/Entity.py
@@ -73,6 +73,12 @@ class Entity:
         for i in range(20):
             classifier_test_result.append(self.test_set.get_tested_classifier(classifier))
             classifier_proba_test_result.append(self.test_set.get_proba_tested_classifier(classifier))
+
+        data_frame_result = pd.DataFrame(classifier_test_result).T
+        data_frame_proba = pd.DataFrame.from_records(classifier_proba_test_result).T
+        all_data_frames = [data_frame_result, data_frame_proba]
+        for df in all_data_frames:
+            df.columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         # Path in this case already have the folder inside de function, and also have the csv extension
-        ToCsv().save_on_csv(name + 'Result' + self.path, pd.DataFrame(classifier_test_result).T)
-        return classifier_test_result, classifier_proba_test_result
+
+        ToCsv().save_on_csv(name + 'Result' + self.path, pd.concat(all_data_frames, axis=1).reset_index(drop=True))
--- src/Main.py
+++ src/Main.py
@@ -1,69 +1,8 @@
-# from src.Entity.BasesObject.Adult import Adult
 from src.Entity.BasesObject.Banana import Banana
-# from src.Entity.BasesObject.Blood import Blood
-# from src.Entity.BasesObject.CTG import Ctg
-# from src.Entity.BasesObject.Diabetes import Diabetes
-# from src.Entity.BasesObject.Ecoli import Ecoli
-# from src.Entity.BasesObject.Faults import Faults
-# from src.Entity.BasesObject.German import German
-# from src.Entity.BasesObject.Glass import Glass
-# from src.Entity.BasesObject.Haberman import Haberman
-# from src.Entity.BasesObject.Heart import Heart
-# from src.Entity.BasesObject.ILPD import Ilpd
-# from src.Entity.BasesObject.Ionosphere import Ionosphere
-# from src.Entity.BasesObject.Laryngeal1 import Laryngeal1
-# from src.Entity.BasesObject.Laryngeal3 import Laryngeal3
-# from src.Entity.BasesObject.Lithuanian import Lithuanian
-# from src.Entity.BasesObject.Liver import Liver
-# from src.Entity.BasesObject.Magic import Magic
-# from src.Entity.BasesObject.Mammo import Mammo
-# from src.Entity.BasesObject.Monk import Monk
-# from src.Entity.BasesObject.Phoneme import Phoneme
-# from src.Entity.BasesObject.Segmentation import Segmentation
-# from src.Entity.BasesObject.Sonar import Sonar
-# from src.Entity.BasesObject.Thyroid import Thyroid
-# from src.Entity.BasesObject.Vehicle import Vehicle
-# from src.Entity.BasesObject.Vertebral import Vertebral
-# from src.Entity.BasesObject.WBC import Wbc
-# from src.Entity.BasesObject.WDVG import Wdvg
-# from src.Entity.BasesObject.Weaning import Weaning
-# from src.Entity.BasesObject.Wine import Wine
 
-
-# adult = Adult()
 banana = Banana()
-# blood = Blood()
-# ctg = Ctg()
-# ecoli = Ecoli()
-# faults = Faults()
-# german = German()
-# diabetes = Diabetes()
-# glass = Glass()
-# haberman = Haberman()
-# heart = Heart()
-# ilpd = Ilpd()
-# ionosphere = Ionosphere()
-# laryngeal1 = Laryngeal1()
-# laryngeal3 = Laryngeal3()
-# lithuanian = Lithuanian()
-# liver = Liver()
-# magic = Magic()
-# mammo = Mammo()
-# monk = Monk()
-# phoneme = Phoneme()
-# segmentation = Segmentation()
-# sonar = Sonar()
-# thyroid = Thyroid()
-# vehicle = Vehicle()
-# vertebral = Vertebral()
-# wbc = Wbc()
-# wdvg = Wdvg()
-# weaning = Weaning()
-# wine = Wine()
 
 knn = banana.get_knn(3)
 best_param, knn = banana.get_knn_best_param(knn)
 banana.get_tested_classifier(knn, "knn")
-# classifier_result, classifier_proba = banana.get_tested_classifier(knn, 'knn')
-# print("Best Param:", best_param)
-
+print("Best Parameters: ", best_param)
