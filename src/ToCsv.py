36c75f5392fd003f9b0a52eda2b4a54ab29545a3
jcanabarro <joaovictorcana@gmail.com>
2018-06-15 11:17:39 -0300

    Add some stuff on the main

--- src/ToCsv.py
+++ src/ToCsv.py
@@ -2,4 +2,4 @@
 class ToCsv:
 
     def save_on_csv(self, path, classifier_result):
-        classifier_result.to_csv('./CsvResult/' + path, sep='\t', encoding='utf-8', index=False)
+        classifier_result.to_csv('../CsvResult/' + path, sep='\t', encoding='utf-8', index=False)
