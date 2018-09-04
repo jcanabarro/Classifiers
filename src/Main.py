234a992663882ba9e6ef8d4d3f83ce3bb789f655
jcanabarro <joaovictorcana@gmail.com>
2018-09-04 11:06:16 -0300

    Adding script to create folders and run all bases

--- src/Main.py
+++ src/Main.py
@@ -1,12 +1,93 @@
-from src.Entity.BasesObject.Adult import Adult
 import numpy as np
+import os
+from src.Entity.BasesObject.Adult import Adult
+from src.Entity.BasesObject.Banana import Banana
+from src.Entity.BasesObject.Blood import Blood
+from src.Entity.BasesObject.CTG import Ctg
+from src.Entity.BasesObject.Diabetes import Diabetes
+from src.Entity.BasesObject.Ecoli import Ecoli
+from src.Entity.BasesObject.Faults import Faults
+from src.Entity.BasesObject.German import German
+from src.Entity.BasesObject.Glass import Glass
+from src.Entity.BasesObject.Haberman import Haberman
+from src.Entity.BasesObject.Heart import Heart
+from src.Entity.BasesObject.ILPD import Ilpd
+from src.Entity.BasesObject.Ionosphere import Ionosphere
+from src.Entity.BasesObject.Laryngeal1 import Laryngeal1
+from src.Entity.BasesObject.Laryngeal3 import Laryngeal3
+from src.Entity.BasesObject.Lithuanian import Lithuanian
+from src.Entity.BasesObject.Liver import Liver
+from src.Entity.BasesObject.Magic import Magic
+from src.Entity.BasesObject.Mammo import Mammo
+from src.Entity.BasesObject.Monk import Monk
+from src.Entity.BasesObject.Phoneme import Phoneme
+from src.Entity.BasesObject.Segmentation import Segmentation
+from src.Entity.BasesObject.Sonar import Sonar
+from src.Entity.BasesObject.Thyroid import Thyroid
+from src.Entity.BasesObject.Vehicle import Vehicle
+from src.Entity.BasesObject.Vertebral import Vertebral
+from src.Entity.BasesObject.WBC import Wbc
+from src.Entity.BasesObject.WDVG import Wdvg
+from src.Entity.BasesObject.Weaning import Weaning
+from src.Entity.BasesObject.Wine import Wine
 
 adult = Adult()
+banana = Banana()
+blood = Blood()
+ctg = Ctg()
+diabetes = Diabetes()
+ecoli = Ecoli()
+faults = Faults()
+german = German()
+glass = Glass()
+haberman = Haberman()
+heart = Heart()
+ilpd = Ilpd()
+ionosphere = Ionosphere()
+laryngeal1 = Laryngeal1()
+laryngeal3 = Laryngeal3()
+lithuanian = Lithuanian()
+liver = Liver()
+magic = Magic()
+mammo = Mammo()
+monk = Monk()
+phoneme = Phoneme()
+segmentation = Segmentation()
+sonar = Sonar()
+thyroid = Thyroid()
+vehicle = Vehicle()
+vertebral = Vertebral()
+wbc = Wbc()
+wdvg = Wdvg()
+weaning = Weaning()
+wine = Wine()
 
-classifiers = adult.get_trained_classifiers(3)
+bases = [adult, banana, blood, ctg, diabetes, ecoli, faults, german, glass, haberman, heart, ilpd, ionosphere,
+             laryngeal1, laryngeal3, lithuanian, liver, magic, mammo, monk, phoneme, segmentation, sonar, thyroid,
+             vehicle, vertebral, wbc, wdvg, weaning, wine]
 
-results = adult.get_borda_rule(classifiers)
+base_name = ['adult', 'banana', 'blood', 'ctg', 'diabetes', 'ecoli', 'faults', 'german', 'glass', 'haberman', 'heart',
+               'ilpd', 'ionosphere', 'laryngeal1', 'laryngeal3', 'lithuanian', 'liver', 'magic', 'mammo', 'monk',
+               'phoneme', 'segmentation', 'sonar', 'thyroid', 'vehicle', 'vertebral', 'wbc', 'wdvg', 'weaning', 'wine']
 
-print(results)
+for base in base_name:
+    classifier_directory = '../ClassifierParam/' + base + '/'
+    result_directory = '../ClassifierResult/' + base + '/'
+    if not os.path.exists(classifier_directory):
+        os.makedirs(classifier_directory)
+    if not os.path.exists(result_directory):
+        os.makedirs(result_directory)
 
-# np.savetxt("../ExecutionResult/resutls.csv", results, delimiter=",", fmt="%f")
+for idx, base in enumerate(bases):
+    proba_final = []
+    for i in range(0, 20):
+        classifiers = base.get_trained_classifiers(3)
+        results, proba = base.get_borda_rule(classifiers)
+        proba_final.append(proba)
+        with open('../ClassifierParam/' + base_name[idx] + '/Classifier' + repr(i) + '.csv', 'w') as f:
+            for item in classifiers:
+                f.write("%s\n" % item.base_estimator.get_params())
+        del classifiers[:]
+        np.savetxt('../ClassifierResult/' + base_name[idx] + '/BordaCounteRule' + repr(i) + '.csv', results, delimiter=",", fmt="%f")
+    print(base_name[idx] + " ", end='')
+    print(np.sum(proba_final) / len(proba_final))
